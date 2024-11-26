dc2ab23c8a66d55a7b84c4e8afeeca1ac293a097
HafidzTheGreat <hafidzjnr106@gmail.com>
2024-11-26 16:07:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var typed = new Typed('.typewriter-text', 
+    {
+
+        strings:[
+            "QUADRAGRAPH",
+        ],
+
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+});
+
+// JavaScript to handle button click
+document.getElementById('enterButton').addEventListener('click', function() {
+    window.location.href = 'pembahasan.html'; // Replace with your desired URL
+});
